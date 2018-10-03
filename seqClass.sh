23281933ea47f9f5fa58909a74f6ca6aa0a86381
pablo.magan <pablo.magan@uvic.cat>
2018-10-03 14:47:09 +0200

    commit explain

--- seqClass.sh
+++ seqClass.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-seq=$1
-seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-if [[ $seq =~ ^[ACGTU]+$ ]]; then
-  if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
-    echo "The sequence is RNA"
-  else
-    echo "The sequence can be DNA or RNA"
+seq=$1 #Crea la variable
+seq=$(echo $seq | tr a-z A-Z)  # Pasa de minúscula a mayúscula
+if [[ $seq =~ ^[ACGTU]+$ ]]; then  #Si el argumento tiene alguno de los valores entrara dentro 
+  if [[ $seq =~ T ]]; then #Si contiene una T entrara dentro
+    echo "The sequence is DNA"  #Mostrara el mensaje
+  elif [[ $seq =~ U ]]; then #Si contiene una T entrara dentro
+    echo "The sequence is RNA" #Mostrara el mensaje
+  else #Si no es una U o T entrara dentro
+    echo "The sequence can be DNA or RNA" #Mostrara el mensaje
   fi
-else
-  echo "The sequence is not DNA nor RNA"
+else #Si no es ninguno de los valors anteriores entrara dentro
+  echo "The sequence is not DNA nor RNA" #Mostrara el mensaje
 fi
